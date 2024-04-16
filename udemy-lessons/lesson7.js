d028f4579a66872add46ba02b595fc2e838d2f35
konstantin.volkov <konstantin.volkov@simbirsoft.com>
2024-04-17 00:47:59 +0200

    finished lesson7

--- udemy-lessons/lesson7.js
+++ udemy-lessons/lesson7.js
@@ -1,1 +1,24 @@
 // Loops
+
+// For i loop
+
+for (let i = 0; i <= 5; i++) {
+  console.log("Hello " + i);
+}
+
+// For of loop
+
+const cars = ["Toyota", "Fiat", "Renauld", "BMW"];
+
+for (let car of cars) {
+  console.log(car);
+  if (car == "Fiat") {
+    break;
+  }
+}
+
+// ES6 syntax for each loop
+
+cars.forEach((car) => {
+  console.log(car);
+});
