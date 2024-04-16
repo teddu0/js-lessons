43e32654bc114a8b02ac1c4c66f917a2cbfb9aeb
konstantin.volkov <konstantin.volkov@simbirsoft.com>
2024-04-17 00:14:37 +0200

    finish lesson6

--- udemy-lessons/lesson6.js
+++ udemy-lessons/lesson6.js
@@ -1,1 +1,14 @@
 // Conditional statements
+
+// Если промеждуток времени от 6 до 12 Goog morning
+// Если промежуток врмени от 12 до 18 Good evening
+// Остальное Good night
+
+const hour = 19;
+if (hour >= 6 && hour < 12) {
+  console.log("Good morning");
+} else if (hour >= 12 && hour < 18) {
+  console.log("Good evening");
+} else {
+  console.log("Good night");
+}
